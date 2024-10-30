bb888e6afbd7bad1764911464139d00455b09d2e
김예린 <yerin.3321@daum.net>
2024-10-30 21:52:03 +0900

    to pagnation update

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../models/Product");
 
+const PAGE_SIZE = 1;
 const productController = {};
 productController.createProduct = async (req, res) => {
   try {
@@ -27,6 +28,31 @@ productController.createProduct = async (req, res) => {
     });
     await product.save();
     res.status(200).json({ status: "success", product });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+productController.getProducts = async (req, res) => {
+  try {
+    const { page, name } = req.query;
+    const cond = name ? { name: { $regex: name, $options: "i" } } : {};
+    let query = Product.find(cond);
+    let response = { status: "success" };
+    if (page) {
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      //최종 몇개 페이지
+      //데이터가 총 몇개 있는지
+      //const totalItemNum = await Product.find(cond).count();
+      const totalItemNum = await Product.countDocuments(cond);
+      // 데이터 총개수/ PAGE_SIZE
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
+
+    const productList = await query.exec();
+    response.data = productList;
+    res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
